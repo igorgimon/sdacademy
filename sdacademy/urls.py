122b34b3a7d57f8ffcd8bca1031e769ecadc928e
igorgimon <igor.red.gimon@gmail.com>
2016-02-13 13:56:10 +0200

    8-2

--- sdacademy/urls.py
+++ sdacademy/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sdacademy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-)
+    url(r'^$', views.index, name='index'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^student_list$', views.student_list, name='student_list'),
+    url(r'^student_detail$', views.student_detail, name='student_detail'),
+#    url(r'^admin/', include(admin.site.urls)),
+#    url(r'^polls/', include('polls.urls', namespace="polls")),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ sdacademy/views.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, '../templates/index.html', {})
+
+def contact(request):
+    return render(request, '../templates/contact.html', {})
+
+def student_list(request):
+    return render(request, '../templates/student_list.html', {})
+
+def student_detail(request):
+    return render(request, '../templates/student_detail.html', {})
+
